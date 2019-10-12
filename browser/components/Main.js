981368fc1c9017c4b307621968db3cb204bf012f
Afia Siaw <afiasiaw@gmail.com>
2019-10-12 12:56:08 -0400

    update state w/ form input

--- browser/components/Main.js
+++ browser/components/Main.js
@@ -16,6 +16,7 @@ export default class Main extends Component {
 
     this.selectStudent = this.selectStudent.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.addStudent = this.addStudent.bind(this);
   }
 
   componentDidMount() {
@@ -45,13 +46,14 @@ export default class Main extends Component {
     });
   }
 
+
   render() {
     console.log('this is the state in main', this.state);
     return (
       <div>
         <h1>Students</h1>
         <button onClick={this.handleClick}>Add Student</button>
-        {this.state.showStudent ? <NewStudentForm /> : null}
+        {this.state.showStudent ? <NewStudentForm addStudent={this.addStudent} /> : null}
         <table>
           <thead>
             <tr>
